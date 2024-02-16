a348533feee62a0b2a4384490cd97487a6dfab01
Job85 <joulescodinglab@gmail.com>
2024-02-16 15:53:08 -0500

    created component and styled container {border: .15rem solid yellowgreen}; h3 {color: yellowgreen}

---
+++ src/app/components/card/Card.jsx
@@ -0,0 +1,10 @@
+import styles from './styles.module.css'
+
+export default function Card() {
+
+    return (
+        <div className={styles.container}>
+            <h3 className={styles.h3}>Card Name</h3>
+        </div>
+    )
+}
