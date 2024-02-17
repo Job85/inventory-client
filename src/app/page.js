81de39e64ab81282d2e2ad1c62decb8840f7f6ed
Job85 <joulescodinglab@gmail.com>
2024-02-16 22:43:56 -0500

    removed Card component import

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,9 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import Card from "./components/card/Card";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <Card />
     </main>
   );
 }
