92e6c74d8aee5736fd3af0f550d216450f45d0fa
Job85 <joulescodinglab@gmail.com>
2024-02-16 22:42:17 -0500

    created more reusable Card component passing title and fields props to LabelInput component

--- src/app/components/card/Card.jsx
+++ src/app/components/card/Card.jsx
@@ -1,10 +1,19 @@
-import styles from './styles.module.css'
+import styles from './styles.module.css';
+import LabelInput from '../input/LabelInputField';
 
-export default function Card() {
+export default function Card({ title, fields }) {
 
     return (
         <div className={styles.container}>
-            <h3 className={styles.h3}>Card Name</h3>
+            <h3 className={styles.h3}>{title}</h3>
+            {fields.map(field => (
+                <LabelInput
+                    key={field.name}
+                    type={field.type}
+                    name={field.name}
+                    placeholder={field.placeholder}
+                />
+            ))}
         </div>
     )
 }
