e19907a24163471d513fbe2f8d3cd2d2bd95c6ee
Job85 <joulescodinglab@gmail.com>
2024-02-17 11:06:47 -0500

    created header with only h1 tag; styled with {border: 3px solid greenyellow} {width: 96vw}

---
+++ src/app/components/header/Header.jsx
@@ -0,0 +1,10 @@
+import styles from './styles.module.css';
+
+export default function Header() {
+
+    return (
+        <div>
+            <h1 className={styles.header1}>Header</h1>
+        </div>
+    )
+}
