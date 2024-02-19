8c42a38233c58bc2cde0b3a96d7c1f6c517c2964
Job85 <joulescodinglab@gmail.com>
2024-02-18 20:52:45 -0500

    added form tag around LabelInput component with style {display: flex; flex-direction: column} removed style {display: flex; flex-direction: column} from container class

--- src/app/components/card/Card.jsx
+++ src/app/components/card/Card.jsx
@@ -1,20 +1,23 @@
 import styles from './styles.module.css';
 import LabelInput from '../input/LabelInputField';
+import Button from '../button/Button';
 
 export default function Card({ title, fields }) {
 
     return (
         <div className={styles.container}>
             <h3 className={styles.h3}>{title}</h3>
-            {fields.map(field => (
-                <LabelInput
-                    key={field.name}
-                    type={field.type}
-                    name={field.name}
-                    name2={field.name2}
-                    placeholder={field.placeholder}
-                />
-            ))}
+            <form className={styles.item_form}>
+                {fields.map(field => (
+                    <LabelInput
+                        key={field.name}
+                        type={field.type}
+                        name={field.name}
+                        name2={field.name2}
+                        placeholder={field.placeholder}
+                    />
+                ))}
+            </form>
         </div>
     )
 }
