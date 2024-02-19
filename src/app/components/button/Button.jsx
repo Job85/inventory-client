a13eb7c9c73b6d7d2f1c54da9eba90802897f21c
Job85 <joulescodinglab@gmail.com>
2024-02-18 21:14:57 -0500

    added className through interpolation to style button through props

--- src/app/components/button/Button.jsx
+++ src/app/components/button/Button.jsx
@@ -1,9 +1,11 @@
+import styles from './styles.module.css';
+
 export default function Button({ onClick, className, disabled, type, children, style, ariaLabel }) {
 
     return (
         <button
+            className={`${styles.button} ${className}`}
             onClick={onClick}
-            className={className}
             disabled={disabled}
             type={type}
             style={style}
