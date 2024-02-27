4d1c080b475c089729da306200195ecbe8cb0305
Job85 <joulescodinglab@gmail.com>
2024-02-26 19:58:45 -0500

    created login form with Card component

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,8 +1,20 @@
-import styles from './login.module.css';
+import styles from '../page.module.css';
+import Card from '../components/card/Card';
 
 export default function Login() {
 
     return (
-        <div>Login Page</div>
+        <>
+            <Card
+                title="Login"
+                fields={[
+                    { type: "text", name: "email", name2: "Email", placeholder: "your@email.com" },
+                    { type: "text", name: "password", name2: "Password", placeholder: "password" },
+                ]}
+                button_fields={[
+                    { type: "submit", name: "login", ariaLabel: "submit_login", children: "Login" }
+                ]}
+            />
+        </>
     )
 }
